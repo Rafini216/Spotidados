c99a31ab4a8b86deff473290c35165633bf93a1f
Rafini216 <rafa216@hotmail.com>
2025-09-29 16:28:36 +0100

    inicio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import Navbar from "../components/navbar.jsx"; 
+import Footer from "@/components/footer.jsx";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+ 
+
+  return  (
+
+    <div className="min-h-screen bg-gray-50 flex flex-col"> 
+      <Navbar/>
+      <main className="container mx-auto px-4 py-8 flex-grow">
+  <Component {...pageProps} />;
+  </main >
+
+  <Footer/>
+  </div>
+  )
 }
